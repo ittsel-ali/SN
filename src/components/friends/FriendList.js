f4c948ae1e4f4eaee41f238de8bf830b1d4dbf64
ittsel-ali <ittsel1@gmail.com>
2019-07-27 19:12:04 -0400

    New FriendList Card
    * This is here FC Notifer registers

--- src/components/friends/FriendList.js
+++ src/components/friends/FriendList.js
@@ -1,17 +1,67 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import {Card, CardBody, CardTitle, CardText} from 'reactstrap'
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Typography from '@material-ui/core/Typography';
+
+import {getFriendList, updateFriendList} from '../../store/actions/friendActions'
+import Notifier from '../../services/Notifier';
 
 class FriendList extends Component{
   constructor(props){
     super(props);
+    
+    this.state = {
+      userID: window.localStorage.getItem("user.token") 
+    };
   }
 
+  componentDidMount(){
+    this.props.updateFreinds();
+
+    const notifier = new Notifier();
+    notifier.subscribeToFCNotifier(this.state.userID, this.props.addNewFriend);
+  }
+
+  displayFriends(){
+    const list = [];
+
+    if( this.props.friends == undefined || this.props.friends.length == 0)
+      list.push(<p>No Friends Available</p>);
+    else
+      for( const friend of this.props.friends){
+        list.push(
+          <ListItem id={friend.id}>
+            <ListItemAvatar>
+              <Avatar src={friend.image}>
+              </Avatar>
+            </ListItemAvatar>
+            
+            <ListItemText id={friend.id} primary={
+              <Typography variant="body2" color="textSecondary" component="p">
+                <a href={`/friend/${friend.id}`} class="active">{friend.name}</a>
+              </Typography>
+            } />
+              
+          </ListItem>
+          );
+        
+      };
+
+    return list;
+  }
   render(){
     return(
-      <div>
-        I am FriendList
-      </div>
+      <List subheader={<ListSubheader>Friends</ListSubheader>}>
+      {this.displayFriends()}  
+      </List>
       );
   }
 }
@@ -19,8 +69,15 @@ class FriendList extends Component{
 
 const mapsToProps = (state) => {
   return({
-    user: state.firebase.profile,
+    friends: state.friendstore.friends
   });
 }
 
-export default connect(mapsToProps)(FriendList)
+const mapsDispatchToProps = (dispatch) => {
+  return {
+    updateFreinds: () => dispatch(getFriendList()),
+    addNewFriend: (user) => dispatch(updateFriendList(user))
+  }
+}
+
+export default connect(mapsToProps, mapsDispatchToProps)(FriendList)
