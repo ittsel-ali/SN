1b22e7297c6eeb6296dfec31cc0ef7303303468b
ittsel-ali <ittsel1@gmail.com>
2019-07-27 19:24:38 -0400

    Modified Posts design using material UI

--- src/components/posts/ShowPost.js
+++ src/components/posts/ShowPost.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import { Card, CardBody, Button, Row } from 'reactstrap';
+import { CardBody, Button, Row } from 'reactstrap';
 
 import {registerCallback} from '../../store/actions/callbackActions'
 import {ShowComment, CreateComment} from '../packages/comment'
+
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+
 
 class ShowPost extends Component{
   constructor(props){
@@ -20,6 +37,8 @@ class ShowPost extends Component{
         image: props.post.image,
         total_comments: props.post.total_comments,
         comments: props.post.comments,
+        author: props.post.author,
+        author_image: props.post.author_image,
       }
     };
   }
@@ -48,24 +67,58 @@ class ShowPost extends Component{
 
   render(){
     return(
-      <Card>
-        <CardBody>
-          {this.state.post.text}
-          <br/>
+      <div className="animated fadeIn mt-3">
+        <Card raised className="card">
+          <CardHeader
+            avatar={
+              <Avatar src={this.state.post.author_image} >
+                LU
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={this.state.post.author}
+            subheader="September 14, 2016"
+          />
+          
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {this.state.post.text}
+            </Typography>
+          </CardContent>
+          
           <img class="img-fluid mx-auto d-block" src={this.state.post.image} />
           
-          <hr/>  
-          <p>{this.state.post.total_comments} Comments</p> 
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {this.state.post.total_comments} Comments
+            </Typography>
+            <IconButton aria-label="share">
+              
+            </IconButton>
+          </CardActions>
           
-          <hr/>
-          {this.displayComments()}
-
-          <hr/>
-          <div>  
-            <CreateComment post={this.state.post} />
-          </div>
-        </CardBody>
-      </Card>
+          <Divider variant="middle" component="hr" />
+          
+          <List>
+            {this.displayComments()}
+            <ListItem alignItems="flex-start">
+            <ListItemText>  
+              <CreateComment post={this.state.post} />
+            </ListItemText>
+            </ListItem>
+          </List> 
+        </Card>
+      </div>
       );
   }
 }
