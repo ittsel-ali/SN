ae128d18df5725ac771d07ee8df38d42cae70c78
ittsel-ali <ittsel1@gmail.com>
2019-07-27 19:19:29 -0400

    Updated Route for friend feature in last commit

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Profile = React.lazy(() => import('./components/users/Profile'));
 const Home = React.lazy(() => import('./components/Home'));
+const FriendProfile = React.lazy(() => import('./components/friends/FriendProfile'));
+
 
 export const routes = [
   { path: '/', exact: true, name: 'Home', component: Home },  // to be updated
   { path: '/profile', name: 'Profile', component: Profile },
+  { path: '/friend/:id', name: 'Friend', component: FriendProfile },
 ];
 
