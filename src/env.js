13b093090ab4ae47d9dc59406055731a0b210206
ittsel-ali <ittsel1@gmail.com>
2019-07-27 18:20:25 -0400

    App environment file (will be changed for security purpose)

--- src/env.js
+++ src/env.js
@@ -1,13 +1,21 @@
 const prod = {
+  keys: {
+    NOTIFIER_KEY: "a89b945f4dec90c0527f"
+  },
+
   url: {
-    API_URL: "https://"
+    API_URL: "https://linkup-backend-rails.herokuapp.com"
   }
 }; 
 
 const dev = {
+  keys: {
+    NOTIFIER_KEY: "a89b945f4dec90c0527f"
+  },
+  
   url: {
     API_URL: "http://localhost:3000"
   }
 };
 
-export const config = process.env.NODE_ENV === 'development' ? dev : prod;
+export const config = process.env.NODE_ENV == 'development' ? dev : prod;
